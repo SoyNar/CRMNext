4ffde79538f3c5cc2647764f780e0b259d5f8a18
SoyNar <narciris.tech@gmail.com>
2026-05-31 09:56:28 -0500

    update: Acctualizando ruta por defecto

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check()
+        ? redirect()->route('clients.index')
+        : redirect()->route('login');
 });
 
 
