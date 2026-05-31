ba0e244a290dd7fa5303ae7bb67e9bc5c98bfaa9
SoyNar <narciris.tech@gmail.com>
2026-05-31 09:33:19 -0500

    fix: wrap top-level await in async function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,29 @@
+// resources/js/app.js
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 import { useAuthStore } from "./stores/auth.js"
 import ClientsList from "./pages/ClientsList.vue"
-import ContactsView from "./pages/ContactsView.vue";
+import ContactsView from "./pages/ContactsView.vue"
 
 const pinia = createPinia()
 
-await fetch('/sanctum/csrf-cookie', { credentials: 'include' })
+async function init() {
+    await fetch('/sanctum/csrf-cookie', { credentials: 'include' })
+    const auth = useAuthStore(pinia)
+    await auth.fetchUser()
 
-const auth = useAuthStore(pinia)
-await auth.fetchUser()
-
-if (!auth.user) {
-    window.location.replace('/login')
-} else {
-    const mounts = [
-        { selector: '#clients-app', component: ClientsList },
-        { selector: '#contacts-app', component: ContactsView },
-    ]
-
-    mounts.forEach(({ selector, component }) => {
-        const el = document.querySelector(selector)
-        if (el) createApp(component).use(pinia).mount(el)
-    })
+    if (!auth.user) {
+        window.location.replace('/login')
+    } else {
+        const mounts = [
+            { selector: '#clients-app', component: ClientsList },
+            { selector: '#contacts-app', component: ContactsView },
+        ]
+        mounts.forEach(({ selector, component }) => {
+            const el = document.querySelector(selector)
+            if (el) createApp(component).use(pinia).mount(el)
+        })
+    }
 }
+
+init()
